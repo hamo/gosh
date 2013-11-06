43ff14b664773adb4606a61b269355ebc829640d
Yang Bai <hamo.by@gmail.com>
2013-11-06 22:29:14 +0800

    add testing for shebang, perl and workdir

--- gosh_test.go
+++ gosh_test.go
@@ -4,6 +4,7 @@ import (
 	"io"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 	"testing"
 )
 
@@ -13,6 +14,9 @@ const (
 
 	shWithShebangArg    = "#!/bin/sh\necho $1"
 	shWithoutShebangArg = "echo $1"
+
+	perlWithShebang    = "#!/usr/bin/perl\nprint \"Hello gosh\\n\";"
+	perlWithoutShebang = "print \"Hello gosh\\n\";"
 )
 
 func generateScript(content string) (string, error) {
@@ -59,4 +63,88 @@ func TestRunShWithArg(t *testing.T) {
 	if ops.Stdout.String() != "Hello gosh\n" {
 		t.Fatalf("Script output wrong: %v", ops.Stdout.String())
 	}
+}
+
+func TestRunShWithShebang(t *testing.T) {
+	path, err := generateScript(shWithShebang)
+	if err != nil {
+		t.Fatalf("Generate sh script failed: %v", err)
+	}
+	defer os.Remove(path)
+
+	ops, _, err := Run(path, "", "", true, []string{})
+	if err != nil {
+		t.Fatalf("Run returns error: %v", err)
+	}
+	if ops.Stdout.String() != "Hello gosh\n" {
+		t.Fatalf("Script output wrong: %v", ops.Stdout.String())
+	}
+}
+
+func TestRunPerlWithoutArg(t *testing.T) {
+	path, err := generateScript(perlWithoutShebang)
+	if err != nil {
+		t.Fatalf("Generate sh script failed: %v", err)
+	}
+	defer os.Remove(path)
+
+	ops, _, err := Run(path, "/usr/bin/perl", "", true, []string{})
+	if err != nil {
+		t.Fatalf("Run returns error: %v", err)
+	}
+	if ops.Stdout.String() != "Hello gosh\n" {
+		t.Fatalf("Script output wrong: %v", ops.Stdout.String())
+	}
+}
+
+func TestRunPerlWithShebang(t *testing.T) {
+	path, err := generateScript(perlWithShebang)
+	if err != nil {
+		t.Fatalf("Generate sh script failed: %v", err)
+	}
+	defer os.Remove(path)
+
+	ops, _, err := Run(path, "", "", true, []string{})
+	if err != nil {
+		t.Fatalf("Run returns error: %v", err)
+	}
+	if ops.Stdout.String() != "Hello gosh\n" {
+		t.Fatalf("Script output wrong: %v", ops.Stdout.String())
+	}
+}
+
+func TestRunShUnderWd(t *testing.T) {
+	tmpdir := filepath.Join(os.TempDir(), "goshdir")
+	err := os.Mkdir(tmpdir, 0755)
+	if err != nil {
+		t.Fatalf("Make temp work dir failed: %v", err)
+	}
+	defer os.Remove(tmpdir)
+
+	contentFile := filepath.Join(tmpdir, "content")
+	f, err := os.Create(contentFile)
+	if err != nil {
+		t.Fatalf("Create content file failed: %v", err)
+	}
+	_, err = f.WriteString("Hello gosh\n")
+	if err != nil {
+		t.Fatalf("Write content to content file failed: %v", err)
+	}
+	defer os.Remove(contentFile)
+	defer f.Close()
+
+	path, err := generateScript("cat content")
+	if err != nil {
+		t.Fatalf("Generate sh script failed: %v", err)
+	}
+	defer os.Remove(path)
+
+	ops, _, err := Run(path, "/bin/sh", tmpdir, true, []string{})
+	if err != nil {
+		t.Fatalf("Run returns error: %v", err)
+	}
+	if ops.Stdout.String() != "Hello gosh\n" {
+		t.Fatalf("Script output wrong: %v", ops.Stdout.String())
+	}
+
 }
